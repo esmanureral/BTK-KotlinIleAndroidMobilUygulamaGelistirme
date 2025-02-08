14369b158b66446e99426b65e3dff8aa1877996d
Esmanur <esmanur2eral@gmail.com>
2025-02-08 23:28:17 +0300

    izinler

--- yemekTarif/app/src/main/java/com/esmanureral/yemektarif/ListeFragment.kt
+++ yemekTarif/app/src/main/java/com/esmanureral/yemektarif/ListeFragment.kt
@@ -1,13 +1,18 @@
 package com.esmanureral.yemektarif
 
 import android.os.Bundle
+import android.renderscript.ScriptGroup.Binding
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.esmanureral.yemektarif.databinding.FragmentListeBinding
 
 
 class ListeFragment : Fragment() {
+    private var _binding:FragmentListeBinding?=null
+    private val binding get()=_binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,11 +23,24 @@ class ListeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_liste, container, false)
+        _binding = FragmentListeBinding.inflate(inflater,container,false)
+        val view=binding.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.floatingActionButton.setOnClickListener{yeniEkle(it)}//bunu yazmadan onClick metodu çalışmaz
+
+
+    }
+    //burası çalıştığında floatingbutona tıklandığında tarifFragmentine geçiş yapılıyor
+    fun yeniEkle(view: View){
+        val action=ListeFragmentDirections.actionListeFragmentToTarifFragment(bilgi = "yeni",id=0)
+        Navigation.findNavController(view).navigate(action)
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding=null
     }
 }
--- yemekTarif/app/src/main/java/com/esmanureral/yemektarif/TarifFragment.kt
+++ yemekTarif/app/src/main/java/com/esmanureral/yemektarif/TarifFragment.kt
@@ -1,12 +1,22 @@
 package com.esmanureral.yemektarif
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.esmanureral.yemektarif.databinding.FragmentListeBinding
+import com.esmanureral.yemektarif.databinding.FragmentTarifBinding
+import com.google.android.material.snackbar.Snackbar
 
 class TarifFragment : Fragment() {
+    private var _binding: FragmentTarifBinding?=null
+    private val binding get()=_binding!!
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +27,63 @@ class TarifFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tarif, container, false)
+        _binding = FragmentTarifBinding.inflate(inflater,container,false)
+        val view=binding.root
+        return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.kaydetButton.setOnClickListener { kaydet(it) }
+        binding.imageView.setOnClickListener{gorselSec(it)}
+        binding.silButton.setOnClickListener { sil(it) }
+
+        arguments?.let {
+           val bilgi= TarifFragmentArgs.fromBundle(it).bilgi
+
+            if(bilgi=="yeni"){//yeni tarif ekleniyorsa sil butonu tıklanmayacak
+                binding.silButton.isEnabled=false
+                binding.kaydetButton.isEnabled=true
+            }
+            else{
+                binding.silButton.isEnabled=true
+                binding.kaydetButton.isEnabled=false
+
+            }
+        }
+
+    }
+
+    fun kaydet(view: View){
+
+    }
+    fun sil(view: View){
+
+    }
+    fun gorselSec(view: View){
+        //READ_EXTERNAL_STORAGE izinini var mı yok mu kontrol ediyoruz izin verilmediyse if içine giriyor.
+        if(ContextCompat.checkSelfPermission(requireContext(),Manifest.permission.READ_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
+           //izin verilmemiş izin istiyoruz
+            if(ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(),Manifest.permission.READ_EXTERNAL_STORAGE)){
+                //snackbar gösterip kullanıcıya neden izin istediğimizi söyleyebiliriz.
+                Snackbar.make(view,"Galeriye ulaşıp görsel seçmemiz gerekiyor!",Snackbar.LENGTH_INDEFINITE).setAction(
+                    "izin ver",
+                    View.OnClickListener {
+                        //izin isteyeceğiz.
+                    }
+                ).show()
+            }
+            else{
+                //izin isteyeceğiz
+            }
+        }
+        else{
+            //izin verilmiş,galeriye gidebilir.
+        }
+
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding=null
+    }
 }
